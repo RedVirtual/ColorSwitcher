073d199fc6f713c8c63eeec28c433afdd9604cda
RedVirtual <redvirtualnovotec@gmail.com>
2019-03-11 12:10:54 +0100

    Implementación

--- ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
+++ ColorSwitcher/ColorSwitcher/MainPage.xaml.cs
@@ -13,5 +13,30 @@ namespace ColorSwitcher
         {
             InitializeComponent();
         }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = redSlider.Value;
+            var green = greenSlider.Value;
+            var blue = blueSlider.Value;
+
+            Color bgColor = new Color(red, green, blue);
+
+            boxColor.BackgroundColor = bgColor;
+
+            lblDisplay.Text = ColorToHex(bgColor);
+        }
+
+        private string ColorToHex(Color bgColor)
+        {
+            int red = (int)(bgColor.R * 255);
+            int green = (int)(bgColor.G * 255);
+            int blue = (int)(bgColor.B * 255);
+            int alpha = (int)(bgColor.A * 255);
+
+            return "Valor Hexadecimal: " + $"#{red:X2}{green:X2}{blue:X2}{alpha:X2}";
+
+
+        }
     }
 }
